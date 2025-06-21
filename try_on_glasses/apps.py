332534b4c57f65f0e51f9809bc8452bd470cbc87
unknown <m7mdmonsef@gmail.com>
2025-06-22 01:15:02 +0300

    Final

--- try_on_glasses/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TryOnGlassesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'try_on_glasses'
--- try_on_glasses/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import upload_and_process
-
-urlpatterns = [
-    path('process/', upload_and_process, name='process_image'),
-]
